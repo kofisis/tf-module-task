70e353ddf586f208a5bdc7470981489abc3a780d
Nana Amako <52378242+kofisis@users.noreply.github.com>
2021-10-18 20:31:47 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,12 +3,7 @@ variable "user" {
     default = ["s3_user"]
     description = "to create an IAM user for the user specified above"
 }
-  
-variable "region" {
-    default = "us-east-1"
-    description = "To specify the region for the infrastructure"
-  
-}
+ 
 
 variable "bucket-name" {
     default = "ergo-task-1"
