a8fd5ea3e6bc453a74ac2b4581a3f18a80738558
Nana Amako <52378242+kofisis@users.noreply.github.com>
2021-10-18 22:12:21 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,8 +3,6 @@ variable "user" {
     default = ["s3_user"]
     description = "to create an IAM user for the user specified above"
 }
-  
-}
 
 variable "bucket-name" {
     default = "ergo-task-1"
